a5f7659cae7293d50abba2d53e97ed926819d2de
nakanori0508 <dd7bt8@bma.biglobe.ne.jp>
2020-11-21 15:08:41 +0900

    ユーザー管理機能実装修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do 
+    # string型は255文字以内制限が自動でかかってる
     # nicknameは255文字以内、平仮名カタカナ漢字を許可
-    validates :nickname,        length: {maximum: 255}
-                                # format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :nickname
 
     # emailは6文字以上255文字以下、文字@文字.文字、DBへの重複登録除外
-    validates :email,           length: {minimum: 6, maximum: 255},
+    validates :email,           length: {minimum: 6},
                                 format: {with: /\A\S+@\S+\.\S+\z/},
                                 uniqueness: { case_sensitive: true }
 
-
-    # パスワードは6文字以上1024文字以下、英数字が入力されていることと、末尾が英数字（大文字小文字の判定はなし）
-    validates :password,        length: {minimum: 6,maximum: 1024},
+    # パスワードは6文字以上255文字以下、英数字が入力されていることと、末尾が英数字（大文字小文字の判定はなし）
+    validates :password,        length: {minimum: 6},
                                 format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
     # 下の名前は255文字以内、漢字と平仮名を許可
     # 苗字は255文字以内、漢字と平仮名を許可
-    with_options length: {maximum: 255},format: {with: /\A[一-龥ぁ-ん]/} do
+    with_options format: {with: /\A[一-龥ぁ-ん]/} do
       validates :firstname
       validates :lastname
     end
@@ -28,8 +27,8 @@ class User < ApplicationRecord
     # 下の名前（カタカナ）は255文字以内、カタカナを許可
     # 苗字（カタカナ）は255文字以内、カタカナを許可
     with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-      validates :firstname_kana,  length: {maximum: 255}
-      validates :lastname_kana,   length: {maximum: 255}
+      validates :firstname_kana
+      validates :lastname_kana
     end
   end
     # 生年月日はyyyy-mm-dd形式
