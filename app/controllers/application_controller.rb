bdb402268d2c34456af4e3d94b0211375f66eaff
nakanori0508 <dd7bt8@bma.biglobe.ne.jp>
2020-12-03 19:34:26 +0900

    Basic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   # deviseによる新規登録のみ、デフォルトではemailとパスワードのカラムしか保存を許可していない。許可するカラムを追加する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname,:firstname,:lastname_kana,:firstname_kana,:birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
